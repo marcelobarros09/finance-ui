fc6125cdeb5f9dc252b35fe1ffb220e2a25bca86
Marcelo Barros <marcelobarros-sms@hotmail.com>
2022-10-01 18:12:33 -0300

    implements logout

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -1,20 +1,30 @@
+import { KeycloakService } from 'keycloak-angular';
+import { Router } from '@angular/router';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { LayoutService } from "./service/app.layout.service";
+import { LayoutService } from './service/app.layout.service';
 
 @Component({
-    selector: 'app-topbar',
-    templateUrl: './app.topbar.component.html'
+  selector: 'app-topbar',
+  templateUrl: './app.topbar.component.html',
 })
 export class AppTopBarComponent {
+  items!: MenuItem[];
 
-    items!: MenuItem[];
+  @ViewChild('menubutton') menuButton!: ElementRef;
 
-    @ViewChild('menubutton') menuButton!: ElementRef;
+  @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
 
-    @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
+  @ViewChild('topbarmenu') menu!: ElementRef;
 
-    @ViewChild('topbarmenu') menu!: ElementRef;
+  constructor(
+    public layoutService: LayoutService,
+    private keycloakService: KeycloakService,
+    private router: Router
+  ) {}
 
-    constructor(public layoutService: LayoutService) { }
+  logout() {
+    this.keycloakService.logout();
+    this.router.navigate(['/']);
+  }
 }
